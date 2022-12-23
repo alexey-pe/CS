6e0de0e306911bc3cc212917852aeea163efa6bb
oleksiy.perro <oleksiy.perro@getinge.com>
2022-12-23 17:11:46 +0200

    Incapsulate keyextractor in the predicate passed by the means of functional composition.

--- SortExpences/main.py
+++ SortExpences/main.py
@@ -15,7 +15,13 @@ def main():
 
     def amount(element):
         return element[1]
-    insertion_sort_desc(expenses, descending, amount)
+
+    cheapfirst = compose(ascending, amount)
+    costlyfirst = compose(descending, amount)
+    chronological = compose(ascending, timestamp)
+    recentfirst = compose(descending, timestamp)
+
+    insertion_sort(expenses, chronological)
 
     total = sum((e[1] for e in expenses))
     print("Total amount: ", total)
@@ -24,43 +30,48 @@ def main():
         print(e)
 
 
-def is_sorted(elements, predicate, keyextractor):
+def compose(predicate, keyextractor):
+    def predicate_on_keys(left, right):
+        return predicate(keyextractor(left), keyextractor(right))
+    return predicate_on_keys
+
+
+def is_sorted(elements, predicate):
     prev = None
     for cur in elements:
-        if prev is not None and predicate(keyextractor(cur), keyextractor(prev)):
+        if prev is not None and predicate(cur, prev):
             return False
         prev = cur
     return True
 
 
-def insertion_sort_desc(elements, predicate, keyextractor):
+def insertion_sort(elements, predicate):
     """Sort the elements provided according to the predicate specified by the attributes obtained by keyextractor."""
     # Special case:  empty sequence.
     k = -1
     assert len(elements[:k + 1]) == len([]), 'Sorting reached none elements.'
-    assert is_sorted(elements[:k + 1], predicate, keyextractor),\
-        'Elements up to and including the element reached must be sorted.'
+    assert is_sorted(elements[:k + 1], predicate), 'Elements up to and including the element reached must be sorted.'
 
     # Special case:  a sequence of a single element.
     if len(elements) > 0:
         k = k + 1  # k == 0
         print('Reached element by index ', k, ' with value ', elements[k])
         assert len(elements[:k + 1]) == 1, 'Sorting reached the front element.'
-        assert is_sorted(elements[:k + 1], predicate, keyextractor), 'A single element is always sorted.'
+        assert is_sorted(elements[:k + 1], predicate), 'A single element is always sorted.'
         print(elements)
 
     for k in range(1, len(elements)):
         val = elements[k]
-        print('Reached element by index ', k, ' with key ', keyextractor(val), ' and value ', val)
-        assert is_sorted(elements[:k], predicate, keyextractor),\
+        print('Reached element by index ', k, ' and value ', val)
+        assert is_sorted(elements[:k], predicate),\
             'Elements up to but excluding the element reached must already be sorted.'
         i = k
-        while i > 0 and predicate(keyextractor(val), keyextractor(elements[i - 1])):
+        while i > 0 and predicate(val, elements[i - 1]):
             elements[i] = elements[i - 1]
             i = i - 1
         assert i >= 0
         elements[i] = val
-        assert is_sorted(elements[:k + 1], predicate, keyextractor),\
+        assert is_sorted(elements[:k + 1], predicate),\
             'Elements up to and including the element reached must be sorted.'
         print(elements)
 
