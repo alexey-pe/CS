c857d017b452c198594ff71dc7b66a32032f037b
oleksiy.perro <oleksiy.perro@getinge.com>
2022-12-23 15:16:06 +0200

    Extract functions, call from main()

--- SortExpences/main.py
+++ SortExpences/main.py
@@ -1,6 +1,14 @@
 import csv
 
 
+def main():
+    total, expenses = read('in.txt')
+    insertion_sort_desc(expenses)
+    print("Total amount: ", total)
+    for e in expenses:
+        print(e)
+
+
 def is_sorted_desc(elements):
     prev = None
     for cur in elements:
@@ -10,48 +18,50 @@ def is_sorted_desc(elements):
     return True
 
 
-infile = open('in.txt')
-reader = csv.reader(infile, delimiter=' ')
-expenses = []
-total = 0.0
-for line in reader:
-    date_month_year = line[0].split('-')
-    year_month_date = '-'.join(reversed(date_month_year))
+def insertion_sort_desc(elements):
+    # Special case:  empty sequence.
+    k = -1
+    assert len(elements[:k + 1]) == len([]), 'Sorting reached none elements.'
+    assert is_sorted_desc(elements[:k + 1]), 'Elements up to and including the element reached must be sorted.'
 
-    timestamp = ' '.join([year_month_date, line[1]])
-    expenses.append(timestamp)
+    # Special case:  a sequence of a single element.
+    if len(elements) > 0:
+        k = k + 1  # k == 0
+        assert len(elements[:k + 1]) == 1, 'Sorting reached the front element.'
+        assert is_sorted_desc(elements[:k + 1]), 'A single element is always sorted.'
 
-    total += float(line[2])
+    for k in range(1, len(elements)):
+        val = elements[k]
+        print('Reached element by index ', k, ' with value ', val)
+        assert is_sorted_desc(elements[:k]), 'Elements up to but excluding the element reached must already be sorted.'
+        i = k
+        while i > 0 and val > elements[i - 1]:
+            elements[i] = elements[i - 1]
+            i = i - 1
+        assert i >= 0
+        elements[i] = val
+        assert is_sorted_desc(elements[:k + 1]), 'Elements up to and including the element reached must be sorted.'
+        print(elements)
 
-print("Total amount: ", total)
-# for e in expenses:
-#     print(e)
+    assert len(elements[:k + 1]) == len(elements), 'Sorting must reach the last element.'
 
-# Special case:  empty sequence.
-k = -1
-assert len(expenses[:k + 1]) == len([]), 'Sorting reached none elements.'
-assert is_sorted_desc(expenses[:k + 1]), 'Elements up to and including the element reached must be sorted.'
 
-# # Special case:  a sequence of a single element.
-if len(expenses) > 0:
-    k = k + 1  # k == 0
-    assert len(expenses[:k + 1]) == 1, 'Sorting reached the front element.'
-    assert is_sorted_desc(expenses[:k + 1]), 'A single element is sorted.'
+def read(filename):
+    infile = open(filename)
+    reader = csv.reader(infile, delimiter=' ')
+    expenses = []
+    total = 0.0
+    for line in reader:
+        date_month_year = line[0].split('-')
+        year_month_date = '-'.join(reversed(date_month_year))
 
-for k in range(1, len(expenses)):
-    val = expenses[k]
-    print('Reached element by index ', k, ' with value ', val)
-    assert is_sorted_desc(expenses[:k]), 'Elements up to but excluding the element reached are already sorted.'
-    i = k
-    while i > 0 and val > expenses[i - 1]:
-        expenses[i] = expenses[i - 1]
-        i = i - 1
-    assert i >= 0
-    expenses[i] = val
-    assert is_sorted_desc(expenses[:k + 1]), 'Elements up to and including the element reached must be sorted.'
-    print(expenses)
+        timestamp = ' '.join([year_month_date, line[1]])
+        expenses.append(timestamp)
 
-assert len(expenses[:k + 1]) == len(expenses), 'Sorting must reach the last element.'
+        total += float(line[2])
 
-for e in expenses:
-    print(e)
+    return total, expenses
+
+
+if __name__ == '__main__':
+    main()
