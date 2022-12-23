4d2a7a216f6e605a5c7f054fcc4d160c049800ae
oleksiy.perro <oleksiy.perro@getinge.com>
2022-12-23 16:38:43 +0200

    Pass a predicate to specify the sort order in a form of a local function.

--- SortExpences/main.py
+++ SortExpences/main.py
@@ -3,46 +3,56 @@ import csv
 
 def main():
     expenses = read('in.txt')
-    insertion_sort_desc(expenses)
-    total = 0.0
-    for e in expenses:
-        total += float(e[1])
-        print(e)
 
+    def descending(left, right):
+        return left > right
+
+    def ascending(left, right):
+        return left < right
+    insertion_sort_desc(expenses, ascending)
+
+    total = sum((e[1] for e in expenses))
     print("Total amount: ", total)
 
-def is_sorted_desc(elements):
+    for e in expenses:
+        print(e)
+
+
+def is_sorted(elements, predicate):
     prev = None
     for cur in elements:
-        if prev is not None and cur > prev:
+        if prev is not None and predicate(cur, prev):
             return False
         prev = cur
     return True
 
 
-def insertion_sort_desc(elements):
+def insertion_sort_desc(elements, predicate):
+    """Sort the elements provided according to the predicate specified."""
     # Special case:  empty sequence.
     k = -1
     assert len(elements[:k + 1]) == len([]), 'Sorting reached none elements.'
-    assert is_sorted_desc(elements[:k + 1]), 'Elements up to and including the element reached must be sorted.'
+    assert is_sorted(elements[:k + 1], predicate), 'Elements up to and including the element reached must be sorted.'
 
     # Special case:  a sequence of a single element.
     if len(elements) > 0:
         k = k + 1  # k == 0
+        print('Reached element by index ', k, ' with value ', elements[k])
         assert len(elements[:k + 1]) == 1, 'Sorting reached the front element.'
-        assert is_sorted_desc(elements[:k + 1]), 'A single element is always sorted.'
+        assert is_sorted(elements[:k + 1], predicate), 'A single element is always sorted.'
+        print(elements)
 
     for k in range(1, len(elements)):
         val = elements[k]
         print('Reached element by index ', k, ' with value ', val)
-        assert is_sorted_desc(elements[:k]), 'Elements up to but excluding the element reached must already be sorted.'
+        assert is_sorted(elements[:k], predicate), 'Elements up to but excluding the element reached must already be sorted.'
         i = k
-        while i > 0 and val > elements[i - 1]:
+        while i > 0 and predicate(val, elements[i - 1]):
             elements[i] = elements[i - 1]
             i = i - 1
         assert i >= 0
         elements[i] = val
-        assert is_sorted_desc(elements[:k + 1]), 'Elements up to and including the element reached must be sorted.'
+        assert is_sorted(elements[:k + 1], predicate), 'Elements up to and including the element reached must be sorted.'
         print(elements)
 
     assert len(elements[:k + 1]) == len(elements), 'Sorting must reach the last element.'
@@ -57,7 +67,7 @@ def read(filename):
         year_month_date = '-'.join(reversed(date_month_year))
 
         timestamp = ' '.join([year_month_date, line[1]])
-        expenses.append((timestamp, line[2]))
+        expenses.append((timestamp, float(line[2])))
 
     return expenses
 
