56e1cc97faa90766075680ce90bd28eef12f2512
oleksiy.perro <oleksiy.perro@getinge.com>
2023-02-25 23:39:34 +0200

    Add a C++ solution and a project for problem A.

---
+++ Contests/2023-02-19--Algotester/A/zenik.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string xStr, yStr;
+    cin >> xStr;
+    cin >> yStr;
+
+    int x = atoi(xStr.c_str());
+    int y = atoi(yStr.c_str());
+
+    auto trick = [](int x) {return sqrt(((x * x + 4) * 14 / (double)7 - 8) * 50) + 47 - 10 * x; };
+    
+    string result = (trick(x) == y) ? "Magic" : "Too much juice";
+
+    cout << result << endl;
+}
