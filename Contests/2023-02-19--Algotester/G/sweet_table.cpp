cbde3955080dd6ecff06a0dbae3a4bf0ecb10bea
oleksiy.perro <oleksiy.perro@getinge.com>
2023-02-26 23:20:18 +0200

    Incapsulate solutino for problem G in a class.

--- Contests/2023-02-19--Algotester/G/sweet_table.cpp
+++ Contests/2023-02-19--Algotester/G/sweet_table.cpp
@@ -1,85 +1,94 @@
 #include <iostream>
 #include <limits>
 #include <stack>
-#include <tuple>
 #include <vector>
 
 using namespace std;
 
-vector<pair<double, double>> s;
-double minDistance2;
-size_t iMin, jMin;
-stack<tuple<double, size_t, size_t>> minDistances2;
+class PointCloud
+{
+private:
+    vector<pair<double, double>> _points;
+    stack<double> _minDistances2;
+    double GetMinPairwiseDistance2();
 
-void InputPosition();
-void PopPosition();
+public:
+    double GetMinPairwiseDistance();
+    void AddPoint(double x, double y);
+    void RemoveMostRecentlyAddedPoint();
+};
 
 int main()
 {
     int n, q;
     cin >> n >> q;
 
-    minDistance2 = numeric_limits<double>::max();
+    PointCloud pointCloud;
     for (int i = 0; i < n; ++i)
     {
-        InputPosition();
+        double x, y;
+        cin >> x >> y;
+        pointCloud.AddPoint(x, y);
     }
-
-    minDistances2.push(make_tuple(minDistance2, iMin, jMin));
 
     for (int j = 0; j < q; ++j)
     {
         int command;
         cin >> command;
 
-        if (!command)
+        if (command == 0)
         {
-            PopPosition();
+            pointCloud.RemoveMostRecentlyAddedPoint();
         }
         else
         {
-            InputPosition();
-            minDistances2.push(make_tuple(minDistance2, iMin, jMin));
+            double x, y;
+            cin >> x >> y;
+            pointCloud.AddPoint(x, y);
         }
 
-        cout << sqrt(get<double>(minDistances2.top())) << endl;
+        cout << pointCloud.GetMinPairwiseDistance() << endl;
     }
 }
 
-void InputPosition()
+double PointCloud::GetMinPairwiseDistance2()
 {
-    const size_t j = s.size();
-    double x2, y2;
-    cin >> x2;
-    cin >> y2;
-
-    for (size_t i = 0; i < s.size(); ++i)
+    if (_minDistances2.empty())
     {
-        const auto [x1, y1] = s[i];
-        const double dx = x2 - x1;
-        const double dy = y2 - y1;
+        return numeric_limits<double>::max();
+    }
+
+    return _minDistances2.top();
+}
+
+double PointCloud::GetMinPairwiseDistance()
+{
+    return sqrt(GetMinPairwiseDistance2());
+}
+
+void PointCloud::AddPoint(double x, double y)
+{
+    double minDistance2 = GetMinPairwiseDistance2();
+
+    for (size_t i = 0; i < _points.size(); ++i)
+    {
+        const auto [x1, y1] = _points[i];
+        const double dx = x - x1;
+        const double dy = y - y1;
         const double distance2 = dx * dx + dy * dy;
 
         if (distance2 < minDistance2)
         {
             minDistance2 = distance2;
-            iMin = i;
-            jMin = j;
         }
     }
 
-    s.emplace_back(make_pair(x2, y2));
+    _points.emplace_back(make_pair(x, y));
+    _minDistances2.push(minDistance2);
 }
 
-void PopPosition()
+void PointCloud::RemoveMostRecentlyAddedPoint()
 {
-    s.pop_back();
-
-    const auto [ignore, i, j] = minDistances2.top();
-    if (s.size() <= i || s.size() <= j)
-    {
-        minDistance2 = numeric_limits<double>::max();
-    }
-
-    minDistances2.pop();
+    _points.pop_back();
+    _minDistances2.pop();
 }
