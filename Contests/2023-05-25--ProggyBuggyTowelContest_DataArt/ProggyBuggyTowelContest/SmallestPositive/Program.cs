55a70af0261fcf639ca864fade47527f54543743
alexei.perro <alexei.perro@gmail.com>
2023-12-18 09:05:59 +0200

    Use `System.Linq` in C# to find the smallest positive element of a sequence.
    (cherry picked from commit 7e291f12bbe83b07fc0c532a1c37206aea871d19)

---
+++ Contests/2023-05-25--ProggyBuggyTowelContest_DataArt/ProggyBuggyTowelContest/SmallestPositive/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace SmallestPositive
+{
+    /// <summary>
+    /// Given a sequence of N integers, finds the smallest positive element of it.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var min = Console.ReadLine()
+                .Split(' ')
+                .Skip(1)
+                .Select(s => int.Parse(s))
+                .Where(i => i > 0)
+                .DefaultIfEmpty()
+                .Min();
+
+            Console.WriteLine(min);
+        }
+    }
+}
