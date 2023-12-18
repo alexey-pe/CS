21a6d10e2c8475aa98ae083394e58ee610f9e7cd
alexei.perro <alexei.perro@gmail.com>
2023-12-18 09:31:56 +0200

    Setup `Smallest Positive` Project in PyCharm.

---
+++ Contests/2023-05-25--ProggyBuggyTowelContest_DataArt/SmallestPositive/main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    # Given a sequence of N integers, find the smallest positive element of it.
+
+    print('Enter sequence:')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
