457b9e8fdf2e014882f1bf962d61451b6ba242ff
alexei.perro <alexei.perro@gmail.com>
2024-02-09 02:07:00 +0200

    Add a docstring and type annotations.

--- Contests/2023-05-25--ProggyBuggyTowelContest_DataArt/SmallestPositive/main.py
+++ Contests/2023-05-25--ProggyBuggyTowelContest_DataArt/SmallestPositive/main.py
@@ -3,9 +3,8 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
 def main():
-    # Given a sequence of N integers, find the smallest positive element of it.
+    """Given a sequence of N integers, find the smallest positive element of it."""
 
     seq = list()
 
@@ -15,11 +14,26 @@ def main():
     except Exception as ex:
         print('Invalid sequence.')
 
+    print(seq)
+
     result = smallest_positive(seq)
     print(f'Smallest positive element: {result}')
 
 
-def smallest_positive(seq):
+def smallest_positive(seq: list) -> int:
+    """Find the smallest positive element of a sequence.
+
+    Parameters
+    ----------
+    seq : list
+        The sequence of integer elements.
+
+    Returns
+    -------
+    int
+        The smallest positive element, or zero
+        if none of the elements are positive.
+    """
     return 0
 
 
