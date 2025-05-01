33e277981412a699984815d7d5575f212da4ce35
alexei.perro <alexei.perro@gmail.com>
2025-05-01 20:59:26 +0300

    Add `MouseControls.Tests` project.

---
+++ ReactiveUI/MouseControls.Tests/MouseControlsViewModelTest.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+
+using MouseControls.ViewModels;
+
+namespace MouseControls.Tests
+{
+    public class MouseControlsViewModelTest
+    {
+        [Fact]
+        public void Given_IsCtrlFlagCheckedFalse_When_CheckedOn_Then_True()
+        {
+            // Arrange.
+            var mouseControlsViewModel = new MouseControlsViewModel();
+            mouseControlsViewModel.IsCtrlFlagChecked = false;
+
+            // Act.
+            mouseControlsViewModel.IsCtrlFlagChecked = true;
+
+            // Assert.
+            Assert.True(mouseControlsViewModel.IsCtrlFlagChecked);
+        }
+
+        [Fact]
+        public void Given_IsCtrlFlagCheckedTrue_When_CheckedOff_Then_False()
+        {
+            // Arrange.
+            var mouseControlsViewModel = new MouseControlsViewModel();
+            mouseControlsViewModel.IsCtrlFlagChecked = true;
+
+            // Act.
+            mouseControlsViewModel.IsCtrlFlagChecked = false;
+
+            // Assert.
+            Assert.False(mouseControlsViewModel.IsCtrlFlagChecked);
+        }
+
+    }
+}
