904942c68aef18b56b07e6dc09565ea4579758c0
alexei.perro <alexei.perro@gmail.com>
2025-05-01 14:11:53 +0300

    Rename MainWindow to Mouse Controls.

--- ReactiveUI/MouseControls/MouseControls.xaml.cs
+++ ReactiveUI/MouseControls/MouseControls.xaml.cs
@@ -18,9 +18,9 @@ namespace MouseControls
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MouseControls : Window
     {
-        public MainWindow()
+        public MouseControls()
         {
             InitializeComponent();
         }
